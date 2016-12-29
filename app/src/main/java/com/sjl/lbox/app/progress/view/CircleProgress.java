6cf8d821a7750c233163d40caa2a878bf8c98762
shenjl <1113225201@qq.com>
2016-12-29 13:53:19 +0800

    圆形自定义进度条

---
+++ app/src/main/java/com/sjl/lbox/app/progress/view/CircleProgress.java
@@ -0,0 +1,26 @@
+package com.sjl.lbox.app.progress.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * CircleProgress
+ *
+ * @author SJL
+ * @date 2016/12/29
+ */
+
+public class CircleProgress extends View {
+    public CircleProgress(Context context) {
+        super(context);
+    }
+
+    public CircleProgress(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleProgress(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
