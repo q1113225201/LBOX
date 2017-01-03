8fd9995e2beda6f720a96d78e8a995d51a4d3ea5
SJL <1113225201@qq.com>
2017-01-03 22:25:18 +0800

    1、Activity界面切换动画

---
+++ app/src/main/java/com/sjl/lbox/app/animate/activityAnim/ActivityAnimActivity.java
@@ -0,0 +1,21 @@
+package com.sjl.lbox.app.animate.activityAnim;
+
+import android.os.Bundle;
+
+import com.sjl.lbox.R;
+import com.sjl.lbox.base.BaseActivity;
+
+/**
+ * Activity切换动画
+ *
+ * @author SJL
+ * @date 2017/1/3 22:06
+ */
+public class ActivityAnimActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anim);
+    }
+}
