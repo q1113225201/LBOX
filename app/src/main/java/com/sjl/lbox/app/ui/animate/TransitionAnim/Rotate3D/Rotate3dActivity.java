178808011cb86387d22b422dc7345855380a75a8
shenjl <1113225201@qq.com>
2017-01-20 17:03:39 +0800

    1、图片3D翻转

--- app/src/main/java/com/sjl/lbox/app/ui/animate/TransitionAnim/Rotate3D/Rotate3dActivity.java
+++ app/src/main/java/com/sjl/lbox/app/ui/animate/TransitionAnim/Rotate3D/Rotate3dActivity.java
@@ -1,5 +1,6 @@
 package com.sjl.lbox.app.ui.animate.TransitionAnim.Rotate3d;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -9,6 +10,7 @@ import android.widget.RelativeLayout;
 
 import com.sjl.lbox.R;
 import com.sjl.lbox.app.ui.animate.TransitionAnim.Rotate3d.anim.Rotate3dAnim;
+import com.sjl.lbox.app.ui.animate.TransitionAnim.Rotate3d.view.Rotate3DView;
 import com.sjl.lbox.base.BaseActivity;
 
 /**
@@ -23,6 +25,10 @@ public class Rotate3dActivity extends BaseActivity implements View.OnClickListen
     private ImageView ivRotate3D1;
     private ImageView ivRotate3D2;
     private RelativeLayout rlRotate3D;
+
+    private Button btnRotateX2;
+    private Button btnRotateY2;
+    private Rotate3DView rotate3DView;
 
     private float centerX;
     private float centerY;
@@ -43,22 +49,45 @@ public class Rotate3dActivity extends BaseActivity implements View.OnClickListen
         ivRotate3D1 = (ImageView) findViewById(R.id.ivRotate3D1);
         ivRotate3D2 = (ImageView) findViewById(R.id.ivRotate3D2);
         rlRotate3D = (RelativeLayout) findViewById(R.id.rlRotate3D);
+
+        btnRotateX2 = (Button) findViewById(R.id.btnRotateX2);
+        btnRotateX2.setOnClickListener(this);
+        btnRotateY2 = (Button) findViewById(R.id.btnRotateY2);
+        btnRotateY2.setOnClickListener(this);
+        rotate3DView = (Rotate3DView) findViewById(R.id.rotate3DView);
+        rotate3DView.addBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.frame1));
+        rotate3DView.addBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.frame3));
+        rotate3DView.addBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.frame6));
     }
 
     @Override
     public void onClick(View v) {
         centerX = rlRotate3D.getWidth() / 2.0f;
         centerY = rlRotate3D.getHeight() / 2.0f;
-        Rotate3dAnim rotate3dAnim = null;
         axial = Rotate3dAnim.AXIAL_Z;
         switch (v.getId()) {
             case R.id.btnRotateX:
                 axial = Rotate3dAnim.AXIAL_X;
+                startRotate3DAnim();
                 break;
             case R.id.btnRotateY:
                 axial = Rotate3dAnim.AXIAL_Y;
+                startRotate3DAnim();
+                break;
+            case R.id.btnRotateX2:
+                rotate3DView.setDirection(Rotate3DView.DIRECTION_X);
+                rotate3DView.toNext();
+                break;
+            case R.id.btnRotateY2:
+                rotate3DView.setDirection(Rotate3DView.DIRECTION_Y);
+                rotate3DView.toNext();
                 break;
         }
+
+    }
+
+    private Rotate3dAnim rotate3dAnim = null;
+    private void startRotate3DAnim() {
         rotate3dAnim = new Rotate3dAnim(360f, 270f, centerX, centerY, 0, axial, true);
         rotate3dAnim.setDuration(3000);
         rotate3dAnim.setFillAfter(true);
