08c4ec30079fd4f3bbb07c8fc084b6730118d55e
shenjl <1113225201@qq.com>
2017-06-06 16:54:48 +0800

    1、来电监听

--- app/src/main/java/com/sjl/lbox/app/component/broadcastreceiver/phone/PhoneStateReceiver.java
+++ app/src/main/java/com/sjl/lbox/app/component/broadcastreceiver/phone/PhoneStateReceiver.java
@@ -6,16 +6,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 
 import com.sjl.lbox.util.LogUtil;
 
 /**
  * 电话监听
- * <uses-permission android:name="android.permission.PROCESS_OUTGOING_CALLS"/>
  * <uses-permission android:name="android.permission.READ_PHONE_STATE" />
  * 过滤器
  * <intent-filter>
- * <action android:name="android.intent.action.NEW_OUTGOING_CALL" />
  * <action android:name="android.intent.action.PHONE_STATE" />
  * </intent-filter>
  *
@@ -25,34 +24,30 @@ import com.sjl.lbox.util.LogUtil;
 public class PhoneStateReceiver extends BroadcastReceiver {
     private static final String TAG = "PhoneStateReceiver";
     public static final String ACTION = "PhoneStateReceiver";
-    private static boolean isCallIn = false;
+    public static String callState = "";
     private static String callNumber = "";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_NEW_OUTGOING_CALL.equalsIgnoreCase(intent.getAction())) {
-            //拨打电话时会发送action为android.intent.action.NEW_OUTGOING_CALL的广播
-            isCallIn = false;
-            String phoneNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-            sendMessage(context, "拨打电话：" + phoneNumber);
-            LogUtil.i(TAG, "intent=" + intent.toString());
-            LogUtil.i(TAG, "Extras=" + intent.getExtras().toString());
-        } else {
-            //电话状态改变时发送action为android.intent.action.PHONE_STATE的广播
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+        if (TextUtils.equals(state, TelephonyManager.EXTRA_STATE_RINGING)) {
             //来电
-            isCallIn = true;
-            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
-            if (TelephonyManager.CALL_STATE_RINGING == telephonyManager.getCallState()) {
-                //响铃
-                callNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-                sendMessage(context, "来电：" + callNumber);
-            } else if (TelephonyManager.CALL_STATE_IDLE == telephonyManager.getCallState()) {
-                //呼叫闲置
-                sendMessage(context, "无状态：" + callNumber);
-            } else if (TelephonyManager.CALL_STATE_OFFHOOK == telephonyManager.getCallState()) {
-                //挂断
-                sendMessage(context, "接起电话：" + callNumber);
+            if (TextUtils.equals(callState, TelephonyManager.EXTRA_STATE_RINGING)) {
+                return;
             }
+            callState = state;
+            callNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            sendMessage(context, "来电：" + callNumber);
+        }else if(TextUtils.equals(state, TelephonyManager.EXTRA_STATE_OFFHOOK)){
+            //接听
+            callState = state;
+            sendMessage(context, "接听电话：" + callNumber);
+        }else if(TextUtils.equals(state,TelephonyManager.EXTRA_STATE_IDLE)){
+            //挂断
+            callState = state;
+            sendMessage(context, "挂断：" + callNumber);
+        }else{
+            callState = state;
         }
     }
 
