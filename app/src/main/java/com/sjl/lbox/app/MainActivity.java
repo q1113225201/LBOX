05eeafc94f5a2873ba98783a817b4773791f602b
shenjl <1113225201@qq.com>
2017-07-14 09:06:49 +0800

    分支2

--- app/src/main/java/com/sjl/lbox/app/MainActivity.java
+++ app/src/main/java/com/sjl/lbox/app/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        //分支2
     }
 
     private void initView() {
