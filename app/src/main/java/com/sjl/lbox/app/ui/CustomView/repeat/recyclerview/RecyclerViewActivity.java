e8ad8b4218f23e8d53cea609698fedc160d4840f
林zero <1113225201@qq.com>
2018-01-23 15:09:51 +0800

    RecyclerView拖动排序

--- app/src/main/java/com/sjl/lbox/app/ui/CustomView/repeat/recyclerview/RecyclerViewActivity.java
+++ app/src/main/java/com/sjl/lbox/app/ui/CustomView/repeat/recyclerview/RecyclerViewActivity.java
@@ -1,15 +1,18 @@
 package com.sjl.lbox.app.ui.CustomView.repeat.recyclerview;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.widget.Button;
 
 import com.sjl.lbox.R;
 import com.sjl.lbox.app.ui.CustomView.repeat.recyclerview.adapter.RVAdapter;
 import com.sjl.lbox.base.BaseActivity;
+import com.sjl.lbox.util.LogUtil;
 import com.sjl.lbox.util.ToastUtil;
 
 import java.util.ArrayList;
@@ -22,7 +25,7 @@ import java.util.List;
  * @date 2016/12/25 15:44
  */
 public class RecyclerViewActivity extends BaseActivity implements View.OnClickListener {
-
+    private static final String TAG = "RecyclerViewActivity";
     private Button btnListView;
     private Button btnGridView;
     private Button btnAdd;
@@ -63,14 +66,74 @@ public class RecyclerViewActivity extends BaseActivity implements View.OnClickLi
             public void onItemClick(View view, int position) {
                 ToastUtil.showToast(mContext, adapter.getItem(position));
             }
+
+            @Override
+            public void onItemLongClick(RVAdapter.ViewHolder viewHolder, int position) {
+                itemTouchHelper.startDrag(viewHolder);
+            }
         });
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        initTouchHelper();
+    }
+
+    private ItemTouchHelper itemTouchHelper;
+
+    /**
+     * 初始化拖动
+     */
+    private void initTouchHelper() {
+        itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.Callback() {
+            @Override
+            public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                if (recyclerView.getLayoutManager() instanceof GridLayoutManager) {
+                    int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+                    return makeMovementFlags(dragFlags, 0);
+                } else {
+                    int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+                    return makeMovementFlags(dragFlags, 0);
+                }
+            }
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                //获取当前要拖动的viewHolder的position
+                int fromPosition = viewHolder.getAdapterPosition();
+                //获取拖动到新位置的position
+                int toPosition = target.getAdapterPosition();
+                //交换位置,参见Collections#swap方法
+                list.set(toPosition,list.set(fromPosition,list.get(toPosition)));
+                adapter.notifyItemMoved(fromPosition,toPosition);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                //滑动
+            }
+
+            @Override
+            public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+                if(actionState!=ItemTouchHelper.ACTION_STATE_IDLE) {
+                    //选中可拖拉的时候背景高亮
+                    viewHolder.itemView.setBackgroundColor(Color.DKGRAY);
+                }
+                super.onSelectedChanged(viewHolder, actionState);
+            }
+
+            @Override
+            public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                super.clearView(recyclerView, viewHolder);
+                //松开的时候背景还原
+                viewHolder.itemView.setBackgroundColor(0);
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
 
     private void initData() {
         list = new ArrayList<String>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             list.add("item" + i);
         }
     }
