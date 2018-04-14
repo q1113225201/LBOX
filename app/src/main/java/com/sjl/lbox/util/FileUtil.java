96544e98047d423e548decb0b4bff4c434fcbe34
林zero <1113225201@qq.com>
2018-04-14 23:08:22 +0800

    添加Assets文件读取

--- app/src/main/java/com/sjl/lbox/util/FileUtil.java
+++ app/src/main/java/com/sjl/lbox/util/FileUtil.java
@@ -787,4 +787,25 @@ public class FileUtil {
             return Uri.fromFile(file);
         }
     }
+    /**
+     * 读取assets下的文件，返回utf-8 String
+     *
+     * @param context
+     * @param fileName
+     * @return
+     */
+    public static String readAssetsFile(Context context, String fileName) {
+        try {
+            InputStream is = context.getAssets().open(fileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String text = new String(buffer, "utf-8");
+            return text;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
